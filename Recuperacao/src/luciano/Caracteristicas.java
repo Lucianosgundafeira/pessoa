34887df3706c4029e656f10b978a9c796ceb30b6
Aluno <Aluno@DESKTOP-LDNVJ6K>
2018-11-09 08:38:54 -0300

    caracte

--- Recuperacao/src/luciano/Caracteristicas.java
+++ Recuperacao/src/luciano/Caracteristicas.java
@@ -1,5 +1,50 @@
 package luciano;
 
-public class Caracteristicas {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
+@Entity
+public class Caracteristicas implements PessoaBeanss {
+	
+	@Id
+	@GeneratedValue(generator="var_seq", strategy=GenerationType.SEQUENCE)
+	@SequenceGenerator(name="var_seq")
+	private Long id;
+	
+	
+//	private int cp;
+	private int despesavariavel;
+
+
+	private int idade;
+	
+	public int getRendaVariavel() {
+		int passeword = 0;
+		return passeword;
+	}
+	public void setRendaVariavel(int idade) {
+		this.idade = idade;
+	}
+	public int getDesdesavariavel() {
+		return despesavariavel;
+	}
+	public void setDesdesavariavel(int desdesavariavel) {
+		this.despesavariavel = desdesavariavel;
+	}
+	
+	
+	@Override
+	public Long getId() {
+		return id;
+	}
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+		
+
+
+}
 }
